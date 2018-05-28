b0f93cd16565268f5664fcced48ba40c4f9bdfbc
rahul kumar <rahul.sanoriaitr@gmail.com>
2018-05-28 18:49:10 +0530

    Ex1

---
+++ Ex1.py
@@ -0,0 +1,7 @@
+print "Hello world"
+print "Hello Again"
+print "i like typing this ."
+print "this is fun."
+print 'Yay ! Printing.'
+print "i'd much rather you 'not'."
+print 'I "said" do not touch this.'
