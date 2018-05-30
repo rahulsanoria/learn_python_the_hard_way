9c7afb47d6ee8129fc548ab917c144f150bc8063
rahul kumar <rahul.sanoriaitr@gmail.com>
2018-05-30 15:35:00 +0530

    Ex23

---
+++ Ex23.py
@@ -0,0 +1,3 @@
+print "1. Finding Python source code for things you need '\n' 2. Reading through the code and looking for fi les. '\n'3. Trying to understand code you find."
+
+print "Now try some of these other sites  launchpad.net  sourceforge.net  freecode.com"
